6d9ff49291c1fb967640d5c7db6997312858857b
Miniver93 <128822580+Miniver93@users.noreply.github.com>
2024-03-24 06:45:23 +0100

    Arreglado bug

--- models/phonebook.js
+++ models/phonebook.js
@@ -5,14 +5,7 @@ const mongoose = require('mongoose')
 //se utiliza para cambiar la configuración de Mongoose relacionada con la estricta validación de consultas.
 mongoose.set('strictQuery', false)
 
-//Cambiamos la configuración de lo que nos devuelve el JSON
-mongoose.set('toJSON', {
-    transform: (document,returnedObject)=>{
-        returnedObject.id=returnedObject._id
-        delete returnedObject.__v
-        delete returnedObject._id
-    }
-})
+
 
 //Creamos un schema
 const phoneBookSchema = new mongoose.Schema({
@@ -20,8 +13,15 @@ const phoneBookSchema = new mongoose.Schema({
     "number": String
 })
 
+//Cambiamos la configuración de lo que nos devuelve el JSON
+phoneBookSchema.set('toJSON', {
+    transform: (document,returnedObject)=>{
+        returnedObject.id=returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject._id
+    }
+})
+
+
 //Creamos una colección en la base de datos
-const Phone = mongoose.model('Phonebook', phoneBookSchema)
-
-
-module.exports = Phone
+module.exports = mongoose.model('Phonebook', phoneBookSchema)
