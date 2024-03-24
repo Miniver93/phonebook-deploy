4c5dc09dc2436cfcbd2dbea80ab511c43e45c1ff
Miniver93 <128822580+Miniver93@users.noreply.github.com>
2024-03-24 20:01:44 +0100

    Añadidas todas las peticiones para la DB

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express=require('express'); //Importamos el modulo express
 
 require('dotenv').config()
-require('./services/createNotes')
+require('./services/createPhones')
+const connectToDatabase = require('./mongo')
+connectToDatabase()
 
-
-const fs=require('fs');
-
-
-const morgan=require('morgan')
-
-
-const app=express(); //Creamos el servidor
+const fs = require('fs');
+const morgan = require('morgan')
+const app = express(); //Creamos el servidor
+const Phone = require('./models/phonebook')
 
     //Creamos un token que luego le añadiremos a nuestro middleware, que contenga un mensaje personalizado según el código que nos de
     morgan.token('message', (request,response)=>{
@@ -37,99 +35,85 @@ app.use(morgan((tokens, request, response)=>{
 app.use(express.static('dist'))
 app.use(express.json()) //Le decimos que mi app use el middleware para parsear JSON
 
-let persons
-//Aquí leo mi archivo json, le paso por parámetros la ruta, el códificador, si no lo asigno devolverá números y un callback
-fs.readFile('./phonebook.json', 'utf8' ,(error,data)=>{
-    
-    if (error) { //Como el archivo lo que me devuelve es un string, tengo que parsear este string en un objeto json
-        console.error("Cannot read JSON file",error);
-        return;
-    }
-    try {
-        persons=JSON.parse(data) //Como el archivo lo que me devuelve es un string, tengo que parsear este string en un objeto json y después de parsearlo lo almaceno en persons, donde contendrá mi agenda
-        console.log("JSON file read successfully");
-
-    } catch (parseError) {
-        console.error("Error parsing JSON:", parseError);
-    }
-})
 
 
 app.get('/api/persons', (request,response)=>{
-    response.send(persons)
+    Phone.find({})
+    .then(phones=>{
+        response.json(phones).end()
+    })
+    .catch(err=>{
+        response.status(404)
+    })
 })
 
-app.get('/info', (request,response)=>{
-    const entries=persons.length
+app.get('/info', async (request,response)=>{
+    let entries
+    const saveEntries = async () =>{
+        entries=await Phone.countDocuments({}) //Me devuelve todos los documentos que tengo en mi colección Phone
+    }
+    await saveEntries()
+
     const date=new Date()
     response.send(`<p>Phonebook has info for ${entries} people</p><br>${date}`)
 })
 
 app.get('/api/persons/:id', (request, response)=>{
-    const id=Number(request.params.id)
-    console.log("id:",id);
+    const {id}=request.params
 
-    const person=persons.find(pid=> pid.id === id)
-
-
-    if(person){
-        response.json(person).end()
-    }{
-        response.status(404).end()
-    }
-
+    Phone.findById(id)
+    .then(phone=>{
+        return phone ? response.json(phone).end() : response.status(404).end()
+    })
+    .catch(err=>console.error(err))
+ 
     
 })
 
 app.delete('/api/persons/:id', (request,response)=>{
-    const id=Number(request.params.id)
-    persons=persons.filter(pid=> pid.id !== id) //Sobreescribeme todos los elementos de mis phonebook, excepto el que no quiero
-    response.status(204).end()
+    const {id}=request.params
+
+    Phone.findByIdAndDelete(id)
+    .then(result=>{
+        response.status(200).send({ message: 'Phone has been deleted'}).end()
+    })
+    .catch(err=>console.error(err))
 })
 
-app.post('/api/persons', (request,response)=>{
-    const person=request.body
-    const personNames=persons.map(person=>person.name)
+app.post('/api/persons', async (request,response)=>{
+    const person = request.body
 
+    const existingPerson = await Phone.findOne({ name: person.name }) //Aquí guardo el nombre de la persona que tenga el mismo nombre del que quiero añadir
 
-    
+    const newPerson = new Phone({
+        name: person.name,
+        number: person.number
+    })
+
     if (!person || !person.name || !person.number) {
         return response.status(400).json({
             error: "No name or phone"
         })
     }
 
-    const ids=persons.map(person=>person.id)
-    const maxIds=Math.max(...ids)
-
-    const newPhone={
-        id: maxIds + 1,
-        name: person.name,
-        number: person.number
-    }
-
-
-    if(personNames.includes(person.name)){
+    //Si existe ya una persona con el mismo nombre
+    if(existingPerson){
         return response.status(400).json({
             error: "The name already exist in the phonebook"
         })
     }else{
-        response.status(201).json(newPhone)
-        persons=[...persons, newPhone]
-
-        //Una vez añadido a personas el nuevo numero, tengo que escribir todos los numeros que tengo ahora mismo en mi agenda
-        fs.writeFile('./phonebook.json', JSON.stringify(persons), (error)=>{
-            if (error) {
-                console.log("Cannot write file");
-            }
+        newPerson.save()
+        .then(savedNote=>{
+            response.json(savedNote)
         })
+        .catch(err=>console.error(err))
     }
     
 })
 
 
 
-const PORT = process.env.PORT ||3001
+const PORT = process.env.PORT
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
 })
--- mongo.js
+++ mongo.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
+require('dotenv').config()
 //Con esto estoy diciendole a mi script, osea este programa, que cuando lo ejecute, cuando ponga node mongo.js, como estoy pasando solo 2 argumentos, me saldrá un console log diciendome que ponga la contraseña
 // if(process.argv.length < 3){
 //     console.log('give password as argument');
@@ -8,9 +9,8 @@ const mongoose = require('mongoose')
 
 // const password = process.argv[2] //Esto sería el argumento 3 que le paso a la terminal
 
-const password = process.env.PASSWORD
 
-const url = `mongodb+srv://josedeveloper93:${password}@cluster0.8s2xepo.mongodb.net/PhonebookDB?retryWrites=true&w=majority&appName=Cluster0`
+const url = process.env.MONGO_DB_URI
 
 //Para esperar a hacer cosas, como guardar una nota etc.., tenemos que esperar a que se conecte a nuestra base de datos, por eso tenemos que crear una función y ponerla como async, para que cuando termine de conectarse ya podamos ejecutar el código que queramos
 
